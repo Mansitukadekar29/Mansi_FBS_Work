ff845f900066af90928a3e27ae26c6bd414e7d33
Mansitukadekar29 <mansitukadekar@gmail.com>
2025-09-23 12:31:13 +0530

    Add files via upload

---
+++ java/Assignment1/UpperLower.java
@@ -0,0 +1,40 @@
+package Assignment1;
+
+public class UpperLower {
+
+	public static void main(String[] args) {
+		char ch='a';
+//	    printf("Enter the value:");
+//	    scanf("%c",&ch);
+	    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'|| ch=='A'|| ch=='E'|| ch=='I' || ch=='O' ||ch=='U')
+	    {
+	        System.out.printf("The alphabet is vowel");
+	    }
+	    else
+	    {
+	    	System.out.printf("The alphabet is consonent");
+	    }
+	   
+//	    if(ch>=97 && ch<=122)
+//	    {
+//	        ch=ch-32;
+//	        System.out.printf("\n%c",ch);
+//	        System.out.printf("\n The character converted into uppercase");
+//
+//	    }
+//	    else if(ch>=65 && ch<=90)
+//	    {
+//	        ch=ch+32;
+//	        System.out.printf("\n%c",ch);
+//	        System.out.printf("\n The character converted into lowercase");
+//	    }
+//	    else if(ch>='0' && ch<='9')
+//	    {
+//	    	System.out.printf("the no is digit");
+//	    }
+//	    else{
+//	    	System.out.printf("It is special character");
+//	    }
+	}
+
+}
